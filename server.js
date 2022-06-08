da3e587b01f9f557f4c32892138705a25a9e3ed0
kondo0602 <kondo8363@gmail.com>
2022-06-08 20:45:16 +0900

    feat: CORS対応サーバの実装

--- server.js
+++ server.js
@@ -27,3 +27,24 @@ appB.use(express.static("staticsB"));
 
 appA.listen(3000);
 appB.listen(3001);
+
+const appC = express();
+
+const allowCrossOriginAccess = (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST");
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+  }
+
+  next();
+};
+
+appC.use(allowCrossOriginAccess);
+
+appC.post("/", (req, res) => {
+  res.status(200).json({ message: "Get Success!" });
+});
+
+appC.listen(3002);
